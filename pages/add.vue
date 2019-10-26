1450b4931c37980f16e0343d22d187ff89c2d891
victor-dosca <victor-dosca@yandex-team.ru>
2019-10-26 06:56:25 +0300

    Changed add page

--- pages/add.vue
+++ pages/add.vue
@@ -1,34 +1,52 @@
 <template>
   <v-row justify="space-around" align="center">
-    <v-col cols="6">
+    <v-col cols="7">
       <v-form>
-        <v-text-field
-            v-model="name"
-            :counter="10"
-            :rules="nameRules"
-            label="Name"
-            outlined
-            required
-          ></v-text-field>
-          <v-row justify="space-between">
-            <v-col cols="6" style="padding: 0">
-              <v-date-picker :min="getDate()"></v-date-picker>
-            </v-col>
-            <v-col cols="6">
-              <v-subheader style="padding: 0">Number of people</v-subheader>
-              <v-text-field
-                v-model="price"
-                label="Price"
-                outlined
-                required
-              ></v-text-field>
-              <v-textarea
+        <v-row>
+          <v-text-field
+              v-model="name"
+              :counter="10"
+              :rules="nameRules"
+              label="Name"
               outlined
-              label="Description"
-              ></v-textarea>
-              <v-btn color="submit" width="100%">Submit</v-btn>
-            </v-col>
-          </v-row>
+              required
+            ></v-text-field>
+        </v-row>
+        <v-row justify="space-between">
+            <v-date-picker :min="getDate()"></v-date-picker>
+          <v-col cols="6" style="padding-bottom: 0">
+            <v-subheader style="padding: 0">Number of people</v-subheader>
+            <v-slider
+              v-model="slider"
+              class="align-center"
+              :max="max"
+              :min="min"
+            >
+              <template v-slot:append>
+                <v-text-field
+                  v-model="slider"
+                  class="mt-0 pt-0"
+                  hide-details
+                  outlined
+                  single-line
+                  type="number"
+                  style="width: 60px"
+                ></v-text-field>
+              </template>
+            </v-slider>
+            <v-text-field
+              v-model="price"
+              label="Price"
+              outlined
+              required
+            ></v-text-field>
+            <v-textarea
+            outlined
+            label="Description"
+            ></v-textarea>
+            <v-btn color="primary" width="100%">Submit</v-btn>
+          </v-col>
+        </v-row>
       </v-form>
     </v-col>
   </v-row>
@@ -37,6 +55,13 @@
 <script>
 
 export default {
+  data() {
+    return {
+      max: 100,
+      min: 0,
+      slider: 0
+    }
+  },
   methods: {
     getDate: () => {
       let date = new Date();
